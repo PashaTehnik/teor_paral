5d967cdb053ce076f55f569af05e059c6dfbd900
Pavel <polovinko.p0112@gmail.com>
2022-02-08 11:45:41 +0700

    gaga

--- main.cpp
+++ main.cpp
@@ -5,11 +5,11 @@ float arr[10000000];
 float sum = 0;
 
 int main() {
-
+#pragma acc kernels
     for(int i = 0; i < 10000000; i++){
         arr[i] = sin(i);
     }
-
+#pragma acc kernels
     for(auto i: arr){
         sum += i;
     }
