a34119290206ed0fccf276eeb8b7b2acf44e41a4
Pavel <polovinko.p0112@gmail.com>
2022-02-08 15:09:45 +0700

    gaga

--- main.cpp
+++ main.cpp
@@ -19,7 +19,7 @@
 // N, tol, max_iters
 
 int main(int argc, char** argv){
-    int N = 0, max_iters = 0;
+    int N = 0, max_iters = 0, iter = 0;
     float tol = 0;
     if (argc != 4){
         printf("incorrect inputs\n");
@@ -30,7 +30,47 @@ int main(int argc, char** argv){
     tol = std::stof(argv[2]);
     max_iters = std::stoi(argv[3]);
 
-    float arr[N][N];
-    printf("all is ok");
+    float a[N][N],
+          newa[N][N],
+          err = tol+1;
+    for(int j = 1; j < N - 1; j++) {
+        for (int i = 1; i < N - 1; i++) {
+            a[i][j] = (float) rand() / (float) (RAND_MAX) * 50 - 25;
+        }
+    }
+    /////////////////////
+
+    while ((iter<max_iters)&&(err > tol)){
+        iter++;
+        err = 0;
+        for(int j = 1; j < N - 1; j++){
+            for(int i = 1; i < N - 1; i++) {
+
+                newa[i][j] = 0.25*(a[i+1][j] + a[i-1][j] + a[i][j+1] + a[i][j-1]);
+
+                err = fmax(err, newa[i][j] - a[i][j]);
+
+            }
+        }
+
+        for(int j = 1; j < N - 1; j++){
+            for(int i = 1; i < N - 1; i++) {
+
+                a[i][j] = newa[i][j];
+
+            }
+        }
+
+    }
+
+    ///////////////////// print
+    printf("\n");
+    for(int j = 1; j < N - 1; j++){
+        for(int i = 1; i < N - 1; i++) {
+            printf("%f  ", a[i][j]);
+        }
+    }
+    printf("\n");
+
     return 0;
 }
