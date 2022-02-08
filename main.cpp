1ec068aca21e082b11ed100b4db341c3064c3f96
Pavel <polovinko.p0112@gmail.com>
2022-02-08 15:33:48 +0700

    a

--- main.cpp
+++ main.cpp
@@ -33,8 +33,8 @@ int main(int argc, char** argv){
     float a[N][N],
           newa[N][N],
           err = tol+1;
-    for(int j = 1; j < N - 1; j++) {
-        for (int i = 1; i < N - 1; i++) {
+    for(int j = 0; j < N; j++) {
+        for (int i = 0; i < N; i++) {
             a[i][j] = (float)((int)(((float) rand() / (float)(RAND_MAX)) * 5) % 5);
         }
     }
