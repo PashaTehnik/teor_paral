694dfd27a39ae2cffeff6c7c4d88541eb288096c
Pavel <polovinko.p0112@gmail.com>
2022-02-08 14:06:23 +0700

    gaga

--- main.cpp
+++ main.cpp
@@ -1,18 +1,25 @@
 #include <iostream>
 #include <math.h>
 
-double arr[10000000];
-double sum = 0;
+//double arr[10000000];
+//double sum = 0;
+//
+//int main() {
+//#pragma acc kernels
+//    for(int i = 0; i < 10000000; i++){
+//        arr[i] = sin((2*M_PI)/10000000*i);
+//    }
+//#pragma acc kernels
+//    for(auto i: arr){
+//        sum += i;
+//    }
+//    printf("\nsum = %f\n\n", sum);
+//    return 0;
+//}
 
-int main() {
-#pragma acc kernels
-    for(int i = 0; i < 10000000; i++){
-        arr[i] = sin((2*M_PI)/10000000*i);
-    }
-#pragma acc kernels
-    for(auto i: arr){
-        sum += i;
-    }
-    printf("\nsum = %f\n\n", sum);
+
+int main(int argc, char** argv){
+    for(int i = 0; i < argc; i++)
+        printf("%d : %s\n",argc,argv[i]);
     return 0;
 }
