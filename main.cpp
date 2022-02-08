098577c53529c43f801dddfd11e51fa1ba4d6c89
Pavel <polovinko.p0112@gmail.com>
2022-02-08 15:23:17 +0700

    a

--- main.cpp
+++ main.cpp
@@ -38,6 +38,17 @@ int main(int argc, char** argv){
             a[i][j] = (float)((int)(((float) rand() / (float)(RAND_MAX)) * 5) % 5);
         }
     }
+    ///////////////////// print
+
+    printf("\n");
+    for(int j = 1; j < N - 1; j++){
+        printf("\n");
+        for(int i = 1; i < N - 1; i++) {
+            printf("%f  ", a[i][j]);
+        }
+    }
+    printf("\n\n\n");
+
     /////////////////////
 
     while ((iter<max_iters)&&(err > tol)){
