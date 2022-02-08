cc107611faf9fee7a513a0e11c4c6e0e3cf0dbd6
Pavel <polovinko.p0112@gmail.com>
2022-02-08 11:36:50 +0700

    gaga

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <math.h>
+
+float arr[10000000];
+float sum = 0;
 
 int main() {
-    
+
+    for(int i = 0; i < 10000000; i++){
+        arr[i] = sin(i);
+    }
+
+    for(auto i: arr){
+        sum += i;
+    }
+
     return 0;
 }
